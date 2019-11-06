08e2d52b3751200883fdb8a0fe6e835721d09f6a
Connor <47433174+ScriptKiddy99@users.noreply.github.com>
2019-11-06 21:07:06 +0000

    Create Get-LastLoggonName.ps1
    Added Get-LatLoggonName code

---
+++ Get-LastLoggonName.ps1
@@ -0,0 +1,17 @@
+# had issues getting Read-Host to work, so to debug the script i used Transcripts
+# Output location C:\Temp\Output.log
+Start-Transcript -Path C:\Temp\Output.log
+
+# Check Registry for last logged on user display name
+$key = [Microsoft.Win32.RegistryKey]::OpenBaseKey([Microsoft.Win32.RegistryHive]::LocalMachine, [Microsoft.Win32.RegistryView]::Registry64)
+$LastLoggedOnDisplayName = $key.OpenSubKey("SOFTWARE\Microsoft\Windows\CurrentVersion\Authentication\LogonUI")
+$Fullname = $LastLoggedOnDisplayName.GetValue(“LastLoggedOnDisplayName”)
+
+# Write User display name to console
+Write-Host Full name : $Fullname
+
+# End transcript
+Stop-Transcript
+
+# Graceful exit
+Exit 0
